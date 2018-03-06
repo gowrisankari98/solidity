a360466c38d79ccce21c6adf28fa9b2da4dabd72
gowrisankari98 <37014654+gowrisankari98@users.noreply.github.com>
2018-03-06 09:57:50 +0530

    Create candidatepro.sol

---
+++ candidatepro.sol
@@ -0,0 +1,29 @@
+pragma solidity^0.4.0;
+contract hr{
+    
+    mapping (uint256=>s)candidates;
+    struct s{
+        uint256 date;
+        string name;
+        uint256 id;
+    }
+    
+    function interview (uint256 id,string name, uint256 date)public {
+        candidates[id].id=id;
+        candidates[id].name=name;
+        candidates[id].date=date;
+    }
+    
+    function dispaly(uint256 id)public constant returns(string,uint256){
+        return(candidates[id].name,candidates[id].date);
+    }
+    function check(uint256 id)public constant returns(string){
+    if(candidates[id].id==id){
+        return("already attended");
+        }
+        else{
+            return("new candidate");
+        }
+        
+    }   
+ }
