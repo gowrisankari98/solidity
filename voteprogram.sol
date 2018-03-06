90eec15024e22f091a6bb1394eae563c5fdcc161
gowrisankari98 <37014654+gowrisankari98@users.noreply.github.com>
2018-03-06 09:57:08 +0530

    Create voteprogram.sol

---
+++ voteprogram.sol
@@ -0,0 +1,44 @@
+pragma solidity^0.4.0;
+contract vote{
+    struct s{
+        uint256 id;
+        string name;
+         uint256 co;
+         }
+    struct publ{
+        uint256 pid;
+        string pname;
+        }
+    mapping (uint256=> s)vo;
+    mapping (uint256=> publ)voters;
+    function candidate(uint256 id,string name)public  {
+        vo[id].id=id;
+        vo[id].name=name;
+    }
+    function dispaly(uint256 id)public constant returns(string,uint256){
+        return(vo[id].name,vo[id].co);
+    }
+    function getuser(uint256 pid,string pname, uint256 id)public check(pid) {
+        voters[pid].pid=pid;
+        voters[pid].pname=pname;
+        vo[id].co+=1;
+    }
+    modifier check(uint256 pid){
+        require(voters[pid].pid!=pid);
+            _;
+        }
+    function finishh()public constant returns(string){
+        if(vo[1].co>vo[2].co){
+                return"candidate 1 wines";
+            }
+           else if(vo[1].co<vo[2].co)
+        {
+            return"candidate 2 wines";
+        }
+        else if(vo[1].co==vo[2].co)                                         
+        {
+            return "Tie situation";
+        }
+        }
+    }
+
